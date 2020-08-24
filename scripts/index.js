061b6cfb356322b9c0de0e0cf18cfa9c62d47579
Harmony Trevena <harmonytrevena@gmail.com>
2020-08-24 16:10:17 -0400

    Added images and ability to handle user input.

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,46 @@
 'use strict'
 
-// const refreshQuote = document.getElementById('clickMe');
-
+// IIFE Function: Runs immediately
+// (function(){
+//   const url = "https://api.chucknorris.io/jokes/random?category=dev";
+//   get(url).then(function(fetchResponse){
+//     chuckSays.innerHTML = fetchResponse.value;
+//   })
+// })();
 
 // refreshQuote.addEventListener('click', function (event) {
 //   event.preventDefault();
-
-//   const newParagraph = document.createElement("p");
-//   newParagraph.innerHTML = additionalText;
-//   bodyContainer.appendChild(newParagraph);
+  // const url = "https://api.chucknorris.io/jokes/random?category=dev";
+  // get(url).then(function(fetchResponse){
+  //   chuckSays.innerHTML = fetchResponse.value;
+  // })
 // });
 
-
 const chuckSays = document.getElementById('chuckSays');
+const refreshQuote = document.getElementById('refreshQuote');
+const submitFormButton = document.getElementById("submitForm");
+const defaultCategory = "dev"
 
-function get() {
-  // Step 1: Fetch the data from API
-  return fetch('https://api.chucknorris.io/jokes/random?category=dev')
-    // Step 2: Return the json() method from the Response
-    .then(function(response) {
-      return response.json()
-    })
-    // Step 3: Return the data from the response.json() method
-    .then(function(data) {
-      return data;
-    });
+const getQuote = (category) => {
+  const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
+
+  get(url).then(function (fetchResponse) {
+      chuckSays.innerHTML = fetchResponse.value;
+  });
 }
 
-// IIFE Function: Runs immediately
-(function(){
-  get().then(function(fetchResponse){
-    chuckSays.innerHTML = fetchResponse.value;
-  })
+refreshQuote.addEventListener('click', function (event) {
+  event.preventDefault();
+  getQuote(defaultCategory);
+});
+
+submitFormButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  const userInput = document.getElementById("categoryInput");
+  const category = userInput.value;
+  getQuote(category);
+});
+
+(function () {
+  getQuote(defaultCategory)
 })();
